872bb1b5fde5afb0f634ad16712c99629de6510f
Obi Patrick <60532479+ObiP1@users.noreply.github.com>
2020-04-14 15:46:52 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -41,3 +41,54 @@ for i in range(60, len(train_data)):
     
 #conversion of x_train and y_train to numpy arrays
 x_train, y_train = np.array(x_train), np.array(y_train)
+
+#Reshapes training data into 3 dimensions for usabilty
+x_train =np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+#Creation of Long Short Term Memory (LSTM) model
+model = Sequential()
+model.add(LSTM(50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(LSTM(50, return_sequences=False))
+model.add(Dense(25))
+model.add(Dense(1))
+
+#Compilation of model
+model.compile(optimizer= 'adam', loss='mean_squared_error')
+
+#Training of model
+model.fit(x_train, y_train, batch_size=1,epochs=1)
+
+#Creation of training data set and new array containing scaled values from 1002 to 1062 (Previous 60 closing price values)
+test_data = scaled_data[length_training_data - 60: , :]
+#Creation of data sets x_test and y_test (Allows test data to use those 60 previous closing values)
+x_test = []
+y_test = newDataset[length_training_data:, :]
+for i in range(60, len(test_data)):
+    x_test.append(test_data[i-60:i, 0])
+    
+#Conversion of data into numpy array
+x_test = np.array(x_test)
+
+#Reshape of test data for usability (Same process as earlier with our training data)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+#Obtain model's predicted price values
+predictedValues = model.predict(x_test)
+predictedValues = scale.inverse_transform(predictedValues)
+
+#Obtain the RMSE (Root Mean Squared Error) (Measures average magnitude of error)
+RMSE = np.sqrt(np.mean(predictedValues - y_test)**2)
+
+#Displays a visualization to see 
+train = newData[:length_training_data]
+valid = newData[length_training_data:]
+valid['Predictions'] = predictedValues
+#Plot visualization
+plt.figure(figsize=(18,9))
+plt.title('Google Stock Market')
+plt.xlabel('Year')
+plt.ylabel('Closing Price in USD', fontsize = 16)
+plt.plot(train['Close'])
+plt.plot(valid[['Close', 'Predictions']])
+plt.legend(['Train', 'Val', 'Predictions'], loc='lower right')
+plt.show()
