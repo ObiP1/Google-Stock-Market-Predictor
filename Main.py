7941331010744c4d00a66d6e2ada7d211ad6be73
Obi Patrick <60532479+ObiP1@users.noreply.github.com>
2020-04-14 13:31:50 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,3 +7,12 @@ from keras.models import Sequential
 from keras.layers import Dense, LSTM
 import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
+
+df = web.DataReader('GOOG', data_source = 'yahoo', start='2015-01-01', end='2020-04-10')
+
+#Creates plot to analyze previous closing values
+plt.figure(figsize=(16,8))
+plt.title('Previous Closing Prices')
+plt.plot(df['Close'])
+plt.xlabel('Year',fontsize=20)
+plt.ylabel('Price of Stock in USD', fontsize=20)
